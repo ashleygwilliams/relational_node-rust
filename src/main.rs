940b923e7213f89465248ba3ea948af828b07048
Ashley Williams <ashley666ashley@gmail.com>
2016-05-14 17:47:34 -0400

    check that argument is passed and file exists

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::env;
+use std::fs::File;
 
 fn main() {
   let filename: String = match env::args().nth(1) {
     Some(filename)  => filename,
-    None               => {
-      println!("No argument passed. Please pass a filename.");
+    None            => {
+      println!("No filename passed. Please pass a filename using `cargo run -- <filename>`.");
       return; 
     },
   };
 
-  println!("{}", filename);
+  
+  match File::open(&filename) {
+    Ok(data)  => data,
+    Err(err)  => {
+      println!("ERR: {}.", err);
+      return;
+    },
+  };
+
+  println!("Parsing {}.", filename);
 }
